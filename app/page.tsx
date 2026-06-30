fac46d428465eafea7ded02b4594b787c6d3e523
AdamNPL1 <nowakowskia43@gmail.com>
2026-06-30 14:00:52 +0200

    Redirect homepage to custom app

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,5 @@
-"use client";
-
-import { Tldraw } from "tldraw";
-import "tldraw/tldraw.css";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-<>
-    {/* 🔥 TOP BAR */}
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        height: 50,
-        zIndex: 9999,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: "0 16px",
-        background: "linear-gradient(90deg, #6a5acd, #7b68ee)",
-        color: "white",
-        fontWeight: "bold",
-      }}
-    >
-      <div>mojatablica</div>
-      <div style={{ display: "flex", gap: 12 }}>
-        <div>Info</div>
-        <div>Zaloguj się</div>
-        <div>Zarejestruj się</div>
-      </div>
-    </div>
-
-    {/* 🧠 BOARD */}
-    <div style={{ position: "fixed", inset: 0 }}>
-      <Tldraw />
-    </div>
-  </>
-);
+  redirect("/custom");
 }
-
