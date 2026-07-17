c423e01b598d8ada152387ec052c5728ccb9a55e
AdamNPL1 <nowakowskia43@gmail.com>
2026-07-17 19:02:45 +0200

    Redirect www to canonical domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,21 @@ import type { NextConfig } from "next";
 import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.scribooapp.com",
+          },
+        ],
+        destination: "https://scribooapp.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withSentryConfig(nextConfig, {
